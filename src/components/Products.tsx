8f6ed547bfad0b923258d4e24173012f19c97a7d
Amit <amit.p@expertappdevs.dev>
2025-12-09 19:02:18 +0530

    product page added

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -5,9 +5,7 @@ import "react-loading-skeleton/dist/skeleton.css";
 import { Link } from "react-router";
 import toast from "react-hot-toast";
 
-const Products = () => {
-
-    interface Product {
+interface Product {
     id: string,
     title: string,
     price: number,
@@ -20,9 +18,134 @@ const Products = () => {
     }
 }
 
+const Loading = () => {
+    return (
+        <>
+            <div className="col-12 py-5 text-center">
+                <Skeleton height={40} width={560} />
+            </div>
+            <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+                <Skeleton height={592} />
+            </div>
+            <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+                <Skeleton height={592} />
+            </div>
+            <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+                <Skeleton height={592} />
+            </div>
+            <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+                <Skeleton height={592} />
+            </div>
+            <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+                <Skeleton height={592} />
+            </div>
+            <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+                <Skeleton height={592} />
+            </div>
+        </>
+    )
+}
 
-    const [data, setData] = useState([]);
-    const [filter, setFilter] = useState(data);
+interface ShowProductsProps {
+    filter: Product[];
+    data: Product[];
+    setFilter: (products: Product[]) => void;
+    filterProduct: (cat: string) => void;
+}
+
+const ShowProducts = ({ filter, data, setFilter, filterProduct }: ShowProductsProps) => {
+    return (
+        <>
+            <div className="buttons text-center py-5">
+                <button
+                    className="btn btn-outline-dark btn-sm m-2"
+                    onClick={() => setFilter(data)}
+                >
+                    All
+                </button>
+                <button
+                    className="btn btn-outline-dark btn-sm m-2"
+                    onClick={() => filterProduct("men's clothing")}
+                >
+                    Men's Clothing
+                </button>
+                <button
+                    className="btn btn-outline-dark btn-sm m-2"
+                    onClick={() => filterProduct("women's clothing")}
+                >
+                    Women's Clothing
+                </button>
+                <button
+                    className="btn btn-outline-dark btn-sm m-2"
+                    onClick={() => filterProduct("jewelery")}
+                >
+                    Jewelery
+                </button>
+                <button
+                    className="btn btn-outline-dark btn-sm m-2"
+                    onClick={() => filterProduct("electronics")}
+                >
+                    Electronics
+                </button>
+            </div>
+
+            {filter.map((product: Product) => {
+                return (
+                    <div
+                        id={product.id}
+                        key={product.id}
+                        className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4"
+                    >
+                        <div className="card text-center h-100" key={product.id}>
+                            <img
+                                className="card-img-top p-3"
+                                src={product.image}
+                                alt="Card"
+                                height={300}
+                            />
+                            <div className="card-body">
+                                <h5 className="card-title">
+                                    {product.title.substring(0, 12)}...
+                                </h5>
+                                <p className="card-text">
+                                    {product.description.substring(0, 90)}...
+                                </p>
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item lead">$ {product.price}</li>
+                                {/* <li className="list-group-item">Dapibus ac facilisis in</li>
+                    <li className="list-group-item">Vestibulum at eros</li> */}
+                            </ul>
+                            <div className="card-body">
+                                <Link
+                                    to={"/product/" + product.id}
+                                    className="btn btn-dark m-1"
+                                >
+                                    Buy Now
+                                </Link>
+                                <button
+                                    className="btn btn-dark m-1"
+                                    onClick={() => {
+                                        toast.success("Added to cart");
+                                        // addProduct(product);
+                                    }}
+                                >
+                                    Add to Cart
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
+        </>
+    );
+};
+
+const Products = () => {
+
+
+    const [data, setData] = useState<Product[]>([]);
+    const [filter, setFilter] = useState<Product[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
 
     // const dispatch = useDispatch()
@@ -54,125 +177,9 @@ const Products = () => {
         };
     }, []);
 
-    const Loading = () => {
-        return (
-            <>
-                <div className="col-12 py-5 text-center">
-                    <Skeleton height={40} width={560} />
-                </div>
-                <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-                    <Skeleton height={592} />
-                </div>
-                <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-                    <Skeleton height={592} />
-                </div>
-                <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-                    <Skeleton height={592} />
-                </div>
-                <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-                    <Skeleton height={592} />
-                </div>
-                <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-                    <Skeleton height={592} />
-                </div>
-                <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-                    <Skeleton height={592} />
-                </div>
-            </>
-        )
-    }
-
     const filterProduct = (cat: string) => {
         const updatedList = data.filter((item: Product) => item.category === cat);
         setFilter(updatedList);
-    };
-
-    const ShowProducts = () => {
-        return (
-            <>
-                <div className="buttons text-center py-5">
-                    <button
-                        className="btn btn-outline-dark btn-sm m-2"
-                        onClick={() => setFilter(data)}
-                    >
-                        All
-                    </button>
-                    <button
-                        className="btn btn-outline-dark btn-sm m-2"
-                        onClick={() => filterProduct("men's clothing")}
-                    >
-                        Men's Clothing
-                    </button>
-                    <button
-                        className="btn btn-outline-dark btn-sm m-2"
-                        onClick={() => filterProduct("women's clothing")}
-                    >
-                        Women's Clothing
-                    </button>
-                    <button
-                        className="btn btn-outline-dark btn-sm m-2"
-                        onClick={() => filterProduct("jewelery")}
-                    >
-                        Jewelery
-                    </button>
-                    <button
-                        className="btn btn-outline-dark btn-sm m-2"
-                        onClick={() => filterProduct("electronics")}
-                    >
-                        Electronics
-                    </button>
-                </div>
-
-                {filter.map((product:Product) => {
-                    return (
-                        <div
-                            id={product.id}
-                            key={product.id}
-                            className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4"
-                        >
-                            <div className="card text-center h-100" key={product.id}>
-                                <img
-                                    className="card-img-top p-3"
-                                    src={product.image}
-                                    alt="Card"
-                                    height={300}
-                                />
-                                <div className="card-body">
-                                    <h5 className="card-title">
-                                        {product.title.substring(0, 12)}...
-                                    </h5>
-                                    <p className="card-text">
-                                        {product.description.substring(0, 90)}...
-                                    </p>
-                                </div>
-                                <ul className="list-group list-group-flush">
-                                    <li className="list-group-item lead">$ {product.price}</li>
-                                    {/* <li className="list-group-item">Dapibus ac facilisis in</li>
-                    <li className="list-group-item">Vestibulum at eros</li> */}
-                                </ul>
-                                <div className="card-body">
-                                    <Link
-                                        to={"/product/" + product.id}
-                                        className="btn btn-dark m-1"
-                                    >
-                                        Buy Now
-                                    </Link>
-                                    <button
-                                        className="btn btn-dark m-1"
-                                        onClick={() => {
-                                            toast.success("Added to cart");
-                                            // addProduct(product);
-                                        }}
-                                    >
-                                        Add to Cart
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    );
-                })}
-            </>
-        );
     };
 
     return (
@@ -185,7 +192,7 @@ const Products = () => {
                     </div>
                 </div>
                 <div className="row justify-content-center">
-                    {loading ? <Loading /> : <ShowProducts />}
+                    {loading ? <Loading /> : <ShowProducts filter={filter} data={data} setFilter={setFilter} filterProduct={filterProduct} />}
                 </div>
             </div>
         </>
