718ce1682e7fbf9a398570772cb6dc2a88aef00e
Amit <amit.p@expertappdevs.dev>
2025-12-09 15:02:29 +0530

    about page content loaded

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,25 @@ import './App.css'
 import Home from './pages/Home'
 import "../node_modules/font-awesome/css/font-awesome.min.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from 'react-router';
+import About from './pages/About';
 
 function App() {
   return (
-    <div className='App'>
-      < Home />
-    </div>
+    <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          {/* <Route path="/product" element={<Products />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="/product/*" element={<PageNotFound />} /> */}
+        </Routes>
   )
 }
 
