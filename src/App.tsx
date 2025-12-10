3247f59a21b0642399642f3b12ddfdc3b84c37cc
Amit <amit.p@expertappdevs.dev>
2025-12-10 17:49:35 +0530

    single product page added and resolved css issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,37 @@ import './App.css'
 import Home from './pages/Home'
 import "../node_modules/font-awesome/css/font-awesome.min.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { Route, Routes } from 'react-router';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ProductsPage from './pages/ProductsPage';
 import PageNotFound from './pages/PageNotFound';
+import ScrollToTop from './components/scrollToTop';
+import Product from './pages/Product';
 
 function App() {
   return (
-    <Routes>
+    <BrowserRouter>
+      <ScrollToTop>
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-           <Route path="/product" element={<ProductsPage />} />
-           <Route path="*" element={<PageNotFound />} /> 
+          <Route path="/product" element={<ProductsPage />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="/product/:id" element={<Product />} />
           {/* <Route path="/product" element={<Products />} />
           <Route path="/product/:id" element={<Product />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/product/*" element={<PageNotFound />} /> */}
         </Routes>
+      </ScrollToTop>
+     </BrowserRouter>
   )
 }
 
---
+++ src/components/scrollToTop.tsx
@@ -0,0 +1,14 @@
+import { useEffect, type PropsWithChildren } from "react";
+import { useLocation } from "react-router-dom";
+
+const ScrollToTop = ({ children }: PropsWithChildren) => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return children || null;
+};
+
+export default ScrollToTop;
