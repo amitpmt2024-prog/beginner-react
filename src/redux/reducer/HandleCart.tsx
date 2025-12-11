3b69b894d24a193ad6ef7be4fd1793681e5816fc
Amit <amit.p@expertappdevs.dev>
2025-12-11 12:01:36 +0530

    redux implementation started

--- src/redux/reducer/HandleCart.tsx
+++ src/redux/reducer/HandleCart.tsx
@@ -1,21 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { Product } from "../../types/Product.type";
+
 // Retrieve initial state from localStorage if available
 const getInitialCart = () => {
   const storedCart = localStorage.getItem("cart");
   return storedCart ? JSON.parse(storedCart) : [];
 };
 
-const handleCart = (state = getInitialCart(), action) => {
+const handleCart = (state = getInitialCart(), action:any) => {
   console.log('aaaaaa',state,action);
   const product = action.payload;
   let updatedCart;
 
   switch (action.type) {
     case "ADDITEM":
-      // Check if product already in cart
-      const exist = state.find((x) => x.id === product.id);
+      { const exist = state.find((x:Product) => x.id === product.id);
       if (exist) {
         // Increase the quantity
-        updatedCart = state.map((x) =>
+        updatedCart = state.map((x:any) =>
           x.id === product.id ? { ...x, qty: x.qty + 1 } : x
         );
       } else {
@@ -23,20 +25,20 @@ const handleCart = (state = getInitialCart(), action) => {
       }
       // Update localStorage
       localStorage.setItem("cart", JSON.stringify(updatedCart));
-      return updatedCart;
+      return updatedCart; }
 
     case "DELITEM":
-      const exist2 = state.find((x) => x.id === product.id);
+      { const exist2 = state.find((x:Product) => x.id === product.id);
       if (exist2.qty === 1) {
-        updatedCart = state.filter((x) => x.id !== exist2.id);
+        updatedCart = state.filter((x:any) => x.id !== exist2.id);
       } else {
-        updatedCart = state.map((x) =>
+        updatedCart = state.map((x:any) =>
           x.id === product.id ? { ...x, qty: x.qty - 1 } : x
         );
       }
       // Update localStorage
       localStorage.setItem("cart", JSON.stringify(updatedCart));
-      return updatedCart;
+      return updatedCart; }
 
     default:
       return state;
