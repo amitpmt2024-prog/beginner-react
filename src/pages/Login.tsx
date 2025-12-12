baf80dde26a874c3214d294382efd838ea55a3cd
Amit <amit.p@expertappdevs.dev>
2025-12-12 16:57:41 +0530

    latest code uploaded

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,50 +1,91 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Link } from "react-router";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { useForm } from "react-hook-form"
+import { ErrorMessage } from "@hookform/error-message"
+
+interface ILoginForm {
+  email: string,
+  password: string
+}
 
 const Login = () => {
-      return (
+
+  const { register, handleSubmit, formState: { errors } } = useForm<ILoginForm>({
+    criteriaMode: "all",
+  })
+  const onSubmit = (data:ILoginForm) => console.log(data)
+
+  return (
     <>
       <Navbar />
-      <div className="container my-3 py-3">
-        <h1 className="text-center">Login</h1>
-        <hr />
-        <div className="row my-4 h-100">
-          <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
-            <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="container my-3 py-3">
+          <h1 className="text-center">Login</h1>
+          <hr />
+          <div className="row my-4 h-100">
+            <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
               <div className="my-3">
                 <label >Email address</label>
                 <input
-                  type="email"
-                  className="form-control"
-                  id="floatingInput"
-                  placeholder="name@example.com"
+                  {...register("email", {
+                    required: "This is required.",
+                    pattern: {
+                      value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                      message: 'Please enter a valid email',
+                    }
+                  })}
+                  className="form-control" placeholder="name@example.com" />
+                <ErrorMessage
+                  errors={errors}
+                  name="email"
+                  render={({ messages }) =>
+                    messages &&
+                    Object.entries(messages).map(([type, message]) => (
+                      <p key={type} className="errorMessage">{message}</p>
+                    ))
+                  }
                 />
+
               </div>
               <div className="my-3">
                 <label >Password</label>
                 <input
-                  type="password"
-                  className="form-control"
-                  id="floatingPassword"
-                  placeholder="Password"
+                  {...register("password", {
+                    required: "This is required.",
+                    pattern: {
+                      value: /^(?=.*[A-Za-z])(?=.*\d).{8,}$/,
+                      message: 'Please enter a minimum eight characters, at least one letter and one number',
+                    }
+                  })}
+                  className="form-control" placeholder="password" />
+                <ErrorMessage 
+                  errors={errors}
+                  name="password"
+                  render={({ messages }) =>
+                    messages &&
+                    Object.entries(messages).map(([type, message]) => (
+                      <p key={type} className="errorMessage">{message}</p>
+                    ))
+                  }
                 />
               </div>
               <div className="my-3">
                 <p>New Here? <Link to="/register" className="text-decoration-underline text-info">Register</Link> </p>
               </div>
               <div className="text-center">
-                <button className="my-2 mx-auto btn btn-dark" type="submit" disabled>
+                <button className="my-2 mx-auto btn btn-dark" type="submit">
                   Login
                 </button>
               </div>
-            </form>
+            </div>
           </div>
         </div>
-      </div>
+      </form>
       <Footer />
     </>
   );
 }
- 
+
 export default Login;
