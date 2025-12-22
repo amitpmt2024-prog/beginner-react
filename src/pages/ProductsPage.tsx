2463b740335aa19ed6d81ebd674dc730ff62f89a
Amit <amit.p@expertappdevs.dev>
2025-12-22 11:42:51 +0530

    Remove all CSS styles from index.css

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,12 +1,14 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Products from "../components/Products";
+import type { Product } from "../types/Product.type";
 
 
 const ProductsPage = () => {
+    const recentViewed:Product[] = [];
     return (<>
         <Navbar />
-        <Products />
+        <Products recentViewed={recentViewed}/>
         <Footer />
     </>);
 }
