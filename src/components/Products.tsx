f1bd6b949cd1b9648e00cc359abfb82d599f9090
Amit <amit.p@expertappdevs.dev>
2025-12-22 13:08:39 +0530

    link added on the products image

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -85,32 +85,36 @@ const ShowProducts = ({ filter, data, setFilter, filterProduct }: ShowProductsPr
                         className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4"
                     >
                         <div className="card text-center h-100" key={product.id}>
-                            <img
-                                className="card-img-top p-3"
-                                src={product.image}
-                                alt="Card"
-                                height={300}
-                            />
-                            <div className="card-body">
-                                <h5 className="card-title">
-                                    {product?.title?.substring(0, 12)}...
-                                </h5>
-                                <p className="card-text">
-                                    {product?.description?.substring(0, 90)}...
-                                </p>
-                            </div>
+                            <Link to={`/product/${product.id}`} className="text-decoration-none text-dark">
+                                <img
+                                    className="card-img-top p-3"
+                                    src={product.image}
+                                    alt="Card"
+                                    height={300}
+                                />
+
+                                <div className="card-body">
+                                    <h5 className="card-title">
+                                        {product?.title?.substring(0, 12)}...
+                                    </h5>
+                                    <p className="card-text">
+                                        {product?.description?.substring(0, 90)}...
+                                    </p>
+                                </div>
+                            </Link>
+
                             <ul className="list-group list-group-flush">
                                 <li className="list-group-item lead">$ {product.price}</li>
-                                {/* <li className="list-group-item">Dapibus ac facilisis in</li>
-                    <li className="list-group-item">Vestibulum at eros</li> */}
                             </ul>
+
                             <div className="card-body">
                                 <Link
-                                    to={"/product/" + product.id}
+                                    to={`/product/${product.id}`}
                                     className="btn btn-dark m-1"
                                 >
                                     Buy Now
                                 </Link>
+
                                 <button
                                     className="btn btn-dark m-1"
                                     onClick={() => {
@@ -122,6 +126,7 @@ const ShowProducts = ({ filter, data, setFilter, filterProduct }: ShowProductsPr
                                 </button>
                             </div>
                         </div>
+
                     </div>
                 );
             })}
@@ -131,13 +136,13 @@ const ShowProducts = ({ filter, data, setFilter, filterProduct }: ShowProductsPr
 
 const Products = ({ recentViewed }: { recentViewed: Product[] }) => {
     // Lazy initialization: use recentViewed if available, otherwise empty array
-    const [data, setData] = useState<Product[]>(() => 
+    const [data, setData] = useState<Product[]>(() =>
         recentViewed && recentViewed.length > 0 ? recentViewed : []
     );
-    const [filter, setFilter] = useState<Product[]>(() => 
+    const [filter, setFilter] = useState<Product[]>(() =>
         recentViewed && recentViewed.length > 0 ? recentViewed : []
     );
-    const [loading, setLoading] = useState<boolean>(() => 
+    const [loading, setLoading] = useState<boolean>(() =>
         !(recentViewed && recentViewed.length > 0)
     );
 
