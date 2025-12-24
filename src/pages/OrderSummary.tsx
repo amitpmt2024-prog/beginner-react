d2211b163e9b5455eb5c248fe327e076fc50b2d6
Amit <amit.p@expertappdevs.dev>
2025-12-24 13:12:33 +0530

    changes

--- src/pages/OrderSummary.tsx
+++ src/pages/OrderSummary.tsx
@@ -1,10 +1,11 @@
+import { useState } from "react";
 import type { Product } from "../types/Product.type";
 import { Link } from "react-router";
 
 const OrderSummary = ({ state }: { state: Product[] }) => {
     let totalItems: number = 0;
     const shipping: number = 50;
-    let subTotal: number = 0;
+    let [subTotal, setSubTotal] = useState(0);
     state.map((item: Product) => {
         subTotal += (item?.price || 0) * (item.qty ?? 0);
         return subTotal;
@@ -14,6 +15,25 @@ const OrderSummary = ({ state }: { state: Product[] }) => {
         return (totalItems += (item.qty ?? 0));
     })
 
+    const setCouponCode = (code: string) => {
+        if(code === "20%OFF") {
+          if(subTotal > 100) {
+            const discount = subTotal * 0.2;
+            setSubTotal(subTotal - discount);
+          } 
+        } else if(code === "500RS OFF") {
+          if(subTotal > 100) {
+            const discount = 500;
+            setSubTotal(subTotal - discount);
+            console.log('aaaaa',subTotal +shipping);
+          } 
+        } 
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCouponCode(e.target.value);
+    }
+    
     return (<>
             <div className="card mb-4">
                 <div className="card-header py-3 bg-light">
@@ -22,11 +42,15 @@ const OrderSummary = ({ state }: { state: Product[] }) => {
                 <div className="card-body">
                     <ul className="list-group list-group-flush">
                         <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">Products ({totalItems})<span>${Math.round(subTotal)}</span></li>
-                        <li className="list-group-item d-flex justify-content-between align-items-center px-0">
+                        <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
                             Shipping
                             <span>${shipping}</span>
                         </li>
-                        <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
+                        <li className="list-group-item d-flex justify-content-between align-items-center px-0">
+                            Coupon Code
+                            <input type="text" className="form-control" placeholder="Enter coupon code" onChange={handleChange}/>
+                        </li>
+                        <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-2 pb-0">
                             <div>
                                 <strong>Total amount</strong>
                             </div>
